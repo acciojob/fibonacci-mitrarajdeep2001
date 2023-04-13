dac2249cd3955b2a622888218e9cbcbef2483869
Rajdeep Mitra <116889292+mitrarajdeep2001@users.noreply.github.com>
2023-04-13 16:50:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function fibonacci(num) {
 // your code here
+
+	let fTerm = 0, lTerm = 1, i = 3, nthTerm;
+	while (i <= num) {
+		nthTerm = fTerm + lTerm;
+		fTerm = lTerm;
+		lTerm = nthTerm;
+		i++;
+	}
 }
 
 module.exports = fibonacci;
